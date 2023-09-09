e175ca0b5a8ab47ab5b6664271da6951b317f3be
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-09 11:59:02 +0530

    initial commit

---
+++ src/main/java/com/nadeeshani/TimelessReads/TimelessReadsApplication.java
@@ -0,0 +1,13 @@
+package com.nadeeshani.TimelessReads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelessReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelessReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nadeeshani/TimelessReads/TimelessReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadeeshani.TimelessReads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelessReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
