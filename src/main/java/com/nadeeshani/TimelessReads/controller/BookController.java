17700291c8e74fdc488e9fc8eead6b3c6eb09563
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-10 19:49:51 +0530

    created the model and repository

--- src/main/java/com/nadeeshani/TimelessReads/controller/BookController.java
+++ src/main/java/com/nadeeshani/TimelessReads/controller/BookController.java
@@ -1,4 +1,5 @@
 package com.nadeeshani.TimelessReads.controller;
 
 public class BookController {
+
 }
--- src/main/java/com/nadeeshani/TimelessReads/repository/BookRepository.java
+++ src/main/java/com/nadeeshani/TimelessReads/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.nadeeshani.TimelessReads.repository;
 
-public interface BookRepository {
+import com.nadeeshani.TimelessReads.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
 }
