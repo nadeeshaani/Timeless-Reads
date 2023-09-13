225636fb865455b37962ee0b1d50b641a649a396
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-13 09:03:18 +0530

    fixed an error in postmapping

--- src/main/java/com/nadeeshani/TimelessReads/service/BookServiceImpl.java
+++ src/main/java/com/nadeeshani/TimelessReads/service/BookServiceImpl.java
@@ -3,13 +3,17 @@ package com.nadeeshani.TimelessReads.service;
 import com.nadeeshani.TimelessReads.model.Book;
 import com.nadeeshani.TimelessReads.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService{
     @Autowired
     private BookRepository bookRepository;
 
     @Override
-    public Book saveBook(Book book) {
+    public Book saveBook(Book book)
+    {
+
         return bookRepository.save(book);
     }
 }
