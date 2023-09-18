1b252842c147b53f1b9a94fb5cf837fcd359670a
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-18 16:16:34 +0530

    added the main form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Appbar from './components/Appbar';
+import Book from './components/Book';
 
 function App() {
   return (
     <div className="App">
      <Appbar/>
+     <Book/>
     </div>
   );
 }
