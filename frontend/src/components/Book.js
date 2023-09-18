5814d074d9db41e3ddc853fbfccc0f705bc62f9e
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-18 16:26:04 +0530

    style changes to form

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Container, Paper } from '@mui/material';
 
 export default function Book() {
+  const paperStyle = {padding:'50px 20px', width:600, margin:"20px auto"}
   return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
     <Box
       component="form"
       sx={{
@@ -16,5 +20,7 @@ export default function Book() {
       <TextField id="outlined-basic" label="Book author" variant="outlined" />
       
     </Box>
+    </Paper>
+    </Container>
   );
 }
