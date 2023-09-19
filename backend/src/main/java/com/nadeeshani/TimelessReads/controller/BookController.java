58eba4231362ecf4f2eeff01d8d6470b32f34387
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-19 09:32:44 +0530

    added the functionalities to send data to database

--- backend/src/main/java/com/nadeeshani/TimelessReads/controller/BookController.java
+++ backend/src/main/java/com/nadeeshani/TimelessReads/controller/BookController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/book")
+@CrossOrigin
 public class BookController {
     @Autowired
     private BookService bookService;
--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Paper } from '@mui/material';
+import { Container, Paper, Button } from '@mui/material';
 
 export default function Book() {
   const paperStyle = {padding:'50px 20px', width:600, margin:"20px auto"}
   const[name, setName] = React.useState('')
   const[author,setAuthor]=React.useState('')
+
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const book={name, author}
+    console.log(book)
+    fetch("http://localhost:8080/book/add",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(book)
+    }).then(()=>{
+      console.log("New Book Added")
+    })
+  }
 
 
   return (
@@ -28,7 +41,7 @@ export default function Book() {
       value={author}
       onChange= {(e)=>setAuthor(e.target.value)}/>
 
-
+<Button variant="contained" color="secondary" onClick={handleClick}>Submit</Button>
       
     </Box>
     {name}
